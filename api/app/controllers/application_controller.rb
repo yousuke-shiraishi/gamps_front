9cfaa63369a2948c46a6e3baefc7ad2ddb48a218
Yosuke Shiraishi <kujirastomack@gmail.com>
2021-10-19 14:43:37 +0900

    Findy用にtypescriptのフロント部分を抜粋

--- api/app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
-        # protect_from_forgery with: :exception
-        skip_before_action :verify_authenticity_token, raise: false
-
-end
--- api/spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    name {"白石"}
-    email {"ecample@com"}
-    birth {Date.parse('1990-10-10')}
-  end
-end
